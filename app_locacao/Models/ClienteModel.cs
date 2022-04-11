ac496581d06bbec50200a0f67f3939771bab6073
Daniel Cintra de Oliveira <cintra.daniel@icloud.com>
2022-04-11 20:41:20 -0300

    Criação do Projeto Front-end

---
+++ app_locacao/Models/ClienteModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace api_locacao.Model
+{
+    public class ClienteModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
--- app_locacao/Startup.cs
+++ app_locacao/Startup.cs
@@ -1,20 +1,15 @@
-using api_locacao.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace api_locacao
+namespace app_locacao
 {
     public class Startup
     {
@@ -28,13 +23,7 @@ namespace api_locacao
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string connStr = Configuration.GetConnectionString("LocacaoDB");
-            services.AddDbContextPool<ApiDbContext>(options => options.UseMySql(connStr, ServerVersion.AutoDetect(connStr)));
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "api_locacao", Version = "v1" });
-            });
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,16 +33,14 @@ namespace api_locacao
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseSwagger();
-            app.UseSwaggerUI(c => {
-
-                c.RoutePrefix = "swagger";
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Locação API v1");
-
-            });
-
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -61,7 +48,9 @@ namespace api_locacao
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
